306f0907f11370205b52c96c5b7153df62089e31
Nishani Chandrasiri <nishani627@gmail.com>
2025-07-28 16:20:33 -0400

    updated

--- myRadioButton.py
+++ myRadioButton.py
@@ -34,16 +34,16 @@ window.mainloop()
 
 
 # Radiobutton property	Property meaning
-# command	        the callback being invoked when the Radiobutton (not the group it belongs to!) changes its state
-# justify	        the same as for Button
-# state	            the same as for Button
-# variable	        an observable IntVar or StringVar variable reflecting the current selection within the Radiobutton’s group; changing the variable’s value automatically changes the selection
-# value	            a unique (inside the group) value identifying the Radiobutton; can be an integer value or a string, and should be compatible with the variable’s type
+# command	            the callback being invoked when the Radiobutton (not the group it belongs to!) changes its state
+# justify	            the same as for Button
+# state	                the same as for Button
+# variable	            an observable IntVar or StringVar variable reflecting the current selection within the Radiobutton’s group; changing the variable’s value automatically changes the selection
+# value	                a unique (inside the group) value identifying the Radiobutton; can be an integer value or a string, and should be compatible with the variable’s type
 # Some of the Radiobutton’s methods are shown here.
 # Note: there is no toggle() method as a single Radiobutton performs such an operation.
 #
 # Radiobutton method	Method role
-# deselect()	unchecks the widget
-# flash()	the same as for Button
-# invoke()	the same as for Button
-# select()	checks the widget
+# deselect()	        unchecks the widget
+# flash()	            the same as for Button
+# invoke()	            the same as for Button
+# select()	            checks the widget
