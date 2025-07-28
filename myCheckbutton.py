9dab79cd381289261c42d5d26649a023429b03e1
Nishani Chandrasiri <nishani627@gmail.com>
2025-07-28 15:43:23 -0400

    updated

--- myCheckbutton.py
+++ myCheckbutton.py
@@ -1,11 +1,11 @@
 # Checkbutton property	Property meaning
-# bd	the checkbutton frame width (default is two pixels)
-# command	the callback being invoked when the checkbutton changes its state
-# justify	the same as for Button
-# state	the same as for Button
-# variable	an observable IntVar variable reflecting the widget’s state; defaultly it’s set to 1 when the checkbutton is checked, and to 0 otherwise
-# offvalue	the non-default value being assigned to a variable when the checkbutton is not checked
-# onvalue	the non-default value being assigned to a variable when the checkbutton is checked
+# bd	                the checkbutton frame width (default is two pixels)
+# command	            the callback being invoked when the checkbutton changes its state
+# justify	            the same as for Button
+# state	                the same as for Button
+# variable	            an observable IntVar variable reflecting the widget’s state; defaultly it’s set to 1 when the checkbutton is checked, and to 0 otherwise
+# offvalue	            the non-default value being assigned to a variable when the checkbutton is not checked
+# onvalue	            the non-default value being assigned to a variable when the checkbutton is checked
 #
 # And now some of its methods:
 #
